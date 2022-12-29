71123db80b932d588f4c46a8f284d953139499fc
Kareem khaled <kareem.khaled1000@gmail.com>
2022-12-29 16:47:31 +0200

    Reformate code and enhance projectstructure

--- Task 4/AppDelegate.swift
+++ Task 4/AppDelegate.swift
@@ -6,7 +6,6 @@
 //
 
 import UIKit
-import MOLH
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -14,7 +13,6 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
-//        MOLH.shared.activate(true)
         return true
     }
 
