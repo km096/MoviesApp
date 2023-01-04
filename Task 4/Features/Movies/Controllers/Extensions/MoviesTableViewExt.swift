7b7f21e8208704276e382f2d9067e7801c0997a2
Kareem khaled <kareem.khaled1000@gmail.com>
2023-01-05 00:41:07 +0200

    Organize code

---
+++ Task 4/Features/Movies/Controllers/Extensions/MoviesTableViewExt.swift
@@ -0,0 +1,29 @@
+//
+//  MoviesTableViewExt.swift
+//  Task 4
+//
+//  Created by ME-MAC on 1/5/23.
+//
+
+import UIKit
+
+class MoviesTableViewExt: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
